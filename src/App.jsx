1cf7f587054ebb519ff29d39f0d9f49cebb34c3e
pantpad <138708449+pantpad@users.noreply.github.com>
2024-03-04 11:20:13 +0100

    Init useState to fetch data from db

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,18 @@ import Header from "./components/Header/Header";
 import PageLayout from "./layout/PageLayout";
 import FoodList from "./components/Food/FoodList/FoodList";
 
+import { useState, useEffect } from "react";
+
 function App() {
+  const [food, setFood] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
   return (
     <>
-      <Header></Header>
+      <Header />
       <PageLayout className={"w-[min(65rem,100vw-2rem)] border border-red-100"}>
-        <FoodList></FoodList>
+        <FoodList />
       </PageLayout>
     </>
   );
