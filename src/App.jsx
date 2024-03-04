99058c5ed30b4037b4b5cef4330e903896e0d64f
pantpad <138708449+pantpad@users.noreply.github.com>
2024-03-04 10:31:25 +0100

    Add tailwind + tailwindPrettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <>
-      <h1>You got this 💪</h1>
+      <h1 className="bg-red-500">You got this 💪</h1>
       <p>Stuck? Not sure how to proceed?</p>
       <p>Don't worry - we've all been there. Let's build it together!</p>
     </>
