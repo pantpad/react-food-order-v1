7717813e31b978c2171410826ffb68780e53095a
pantpad <138708449+pantpad@users.noreply.github.com>
2024-03-04 10:35:08 +0100

    Add header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
+import Header from "./components/Header/Header";
+
 function App() {
   return (
     <>
-      <h1 className="bg-red-500">You got this 💪</h1>
-      <p>Stuck? Not sure how to proceed?</p>
-      <p>Don't worry - we've all been there. Let's build it together!</p>
+      <Header></Header>
+      <h1>Food Order App</h1>
     </>
   );
 }
