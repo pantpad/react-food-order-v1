9179921fdbe13e2386fb79235b287e7280604d8e
pantpad <138708449+pantpad@users.noreply.github.com>
2024-03-06 10:59:08 +0100

    Refactor CartSuccess component

--- src/components/Cart/CartSuccess.jsx
+++ src/components/Cart/CartSuccess.jsx
@@ -5,7 +5,14 @@ export default function CartSuccess() {
   const { closeModal } = useContext(modalContext);
   return (
     <>
-      <h1>SUCCESS</h1>
+      <section className="text-left">
+        <h1>SUCCESS!</h1>
+        <p className="mt-2">Your order was submitted successfully.</p>
+        <p className="mt-2">
+          We will get back to you with more details via email within the next
+          few minutes.
+        </p>
+      </section>
       <section className="mt-4 flex justify-end gap-2 [&_button]:rounded-md [&_button]:border [&_button]:border-black/25 [&_button]:p-2">
         <button className="bg-amber-300" onClick={closeModal}>
           Okay
